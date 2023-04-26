11767734f94a5412331e6b5dc2ddb1ba0de62622
Sam Nystrom <sam@samnystrom.dev>
2023-04-26 17:41:16 -0400

    refactor: clean up generate.sh

--- generate.sh
+++ generate.sh
@@ -1,12 +1,10 @@
-#!/bin/sh
-
-set -eu
+#!/bin/sh -eu
 
 PALETTE=$(curl -s https://raw.githubusercontent.com/catppuccin/palette/main/palette-porcelain.json)
 
 for flavour in latte frappe macchiato mocha; do
-  echo "$PALETTE" \
-  | gojq --arg flavour "$flavour" -r \
-    '.[$flavour] | to_entries | map("s/$\(.key)/#\(.value.hex)/g;") | add' \
-  | sed -f - template > dist/catppuccin-"$flavour"
+  echo "$PALETTE" |
+    gojq -r --arg flavour "$flavour" \
+      '.[$flavour] | to_entries | map("s/$\(.key)/#\(.value.hex)/g;") | add' |
+    sed -f - template >./dist/catppuccin-"$flavour"
 done
